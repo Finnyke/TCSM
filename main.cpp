9381dd561222599a02abc89a08cce88346b18348
Maxim Pupykin <58301278+Finnyke@users.noreply.github.com>
2021-12-02 23:53:48 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "TCSM.h"
+
+int main() {
+	try {
+		telComSys t(30, 1, 0.1);
+		t.appendToQueue(elTypes::RTSG);
+		t.appendToQueue(elTypes::MPCH);
+		t.appendToQueue(elTypes::AWGNG);
+		t.run();
+		t.printSignal();
+	}
+	catch (const char* str) {
+		cout << "Runtime error:" << endl;
+		cout << str << endl;
+	}
+
+	return 0;
+}
